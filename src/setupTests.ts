aacd60cf9251602cfdcb509124e59942f8fd2e6e
AMANPATRE33 <amanpatre33@gmail.com>
2026-03-22 12:22:21 +0530

    update

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,8 +1,1 @@
 import '@testing-library/jest-dom';
-import { expect, afterEach } from 'vitest';
-import { cleanup } from '@testing-library/react';
-
-// Run cleanup after each test case (e.g., clearing the DOM)
-afterEach(() => {
-  cleanup();
-});
